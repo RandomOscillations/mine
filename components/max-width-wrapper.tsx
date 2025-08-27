63c1b5af9a618445f803b235a00b543d5e036a28
Divnoor <divnoorsingh4602@gmail.com>
2025-08-27 00:22:51 -0700

    fix: update MaxWidthWrapper to use min-height for improved layout consistency and padding adjustments

--- components/max-width-wrapper.tsx
+++ components/max-width-wrapper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const MaxWidthWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <main className="mx-auto max-w-3xl border-x border-dashed border-[var(--color-main-border)] h-[calc(100vh-6.25rem)] px-5 pt-3 pb-0 relative">
+      <main className="mx-auto max-w-3xl border-x border-dashed border-[var(--color-main-border)] min-h-[calc(100vh-6.25rem)] px-5 py-3 pb-8 relative">
         {children}
       </main>
     </>
